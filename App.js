9b6eed388f9eeca7485ce38ad3f3b1ebeee50834
colakalpeer <colakalpeer93@gmail.com>
2024-04-19 23:57:22 +0300

    feat: improvement made

--- App.js
+++ App.js
@@ -1,52 +1,44 @@
 // Variables
 
-const addTask = document.getElementById('add-task');
-const taskContainer = document.getElementById('task-container');
-const inputTask = document.getElementById('input-task');
+const addTask = document.getElementById("add-task");
+const taskContainer = document.getElementById("task-container");
+const inputTask = document.getElementById("input-task");
 
 // Event Listener for add Button
 
-addTask.addEventListener('click', function(){
+addTask.addEventListener("click", function () {
+  let task = document.createElement("div");
+  task.classList.add("task");
 
-    let task = document.createElement('div');
-    task.classList.add('task');
+  let li = document.createElement("li");
+  li.innerText = `${inputTask.value}`;
+  task.appendChild(li);
 
-    let li = document.createElement('li');
-    li.innerText = `${inputTask.value}`;
-    task.appendChild(li);
+  let checkButton = document.createElement("button");
+  checkButton.innerHTML = '<i class="fa-solid fa-check"></i>';
+  checkButton.classList.add("checkTask");
+  task.appendChild(checkButton);
 
-    let checkButton = document.createElement("button");
-    checkButton.innerHTML = '<i class="fa-solid fa-check"></i>';
-    checkButton.classList.add('checkTask');
-    task.appendChild(checkButton);
+  let deleteButton = document.createElement("button");
+  deleteButton.innerHTML = '<i class="fa-solid fa-trash-can"></i>';
+  deleteButton.classList.add("deleteTask");
+  task.appendChild(deleteButton);
 
-    let deleteButton = document.createElement("button");
-    deleteButton.innerHTML = '<i class="fa-solid fa-trash-can"></i>';
-    deleteButton.classList.add('deleteTask');
-    task.appendChild(deleteButton);
+  if (inputTask.value === "") {
+    alert("Please Enter a Task");
+  } else {
+    taskContainer.appendChild(task);
+  }
 
-    if(inputTask.value === "") {
-        alert('Please Enter a Task');
-    } else {
-        taskContainer.appendChild(task);
-    }
+  inputTask.value = "";
 
-    inputTask.value = "";
+  checkButton.addEventListener("click", function () {
+    checkButton.parentElement.style.textDecoration = "line-through";
+  });
 
+  deleteButton.addEventListener("click", function (e) {
+    let target = e.target;
 
-    checkButton.addEventListener('click', function(){
-
-        checkButton.parentElement.style.textDecoration = "line-through";
-
-    });
-
-    deleteButton.addEventListener('click', function(e){
-
-        let target = e.target;
-
-        target.parentElement.parentElement.remove();
-
-    });
-
-
+    target.parentElement.parentElement.remove();
+  });
 });
